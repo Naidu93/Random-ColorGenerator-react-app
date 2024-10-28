4e791e3327029300c6f6cc37fb8bab54c1f55cb2
Naidu <brahmanaidum93@gmail.com>
2024-10-28 21:09:38 +0530

    errors resolved

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import RandomColorGenerator from './components/RandomColorGenerator.jsx'
-import StarRating from './components/StarRating.jsx'
+
+
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  <>
-    <RandomColorGenerator />
-
-    <StarRating noOfStars={10} />
-  </>
-  // </StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
